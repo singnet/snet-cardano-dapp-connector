f686cc00504bfbdfc1ca42b70144488ebc748313
shyamkhokhariya <shyamkhokhariya@gmail.com>
2022-11-14 12:42:39 +0530

    Adding dist folder to test locally

---
+++ dist/index.d.ts
@@ -0,0 +1,18 @@
+declare global {
+    interface Window {
+        cardano: any;
+    }
+}
+export declare const useInjectableWalletHook: (supportingWallets: string[], expectedNetworkId: number | string) => {
+    connectWallet: (walletName: string) => Promise<any>;
+    getChangeAddress: () => Promise<string>;
+    getTokensAndBalance: (walletIdentifier: string) => Promise<any>;
+    supportedWallets: any[];
+    transferTokens: (walletName: string, transferWalletAddress: string, assetPolicyIdHex: string, assetNameHex: string, assetQuantity: string) => Promise<any>;
+    transferTokensWithMatadata: (walletName: string, transferWalletAddress: string, assetQuantity: string, matadata: any) => Promise<any>;
+    detectCardanoInjectableWallets: () => number;
+    getBalanceByPolicyScriptId: (walletIdentifier: string, policyScriptId: string) => Promise<any>;
+    getUsedAddresses: () => Promise<any>;
+    selectedWallet: string | null;
+    selectedNetwork: number | null;
+};
